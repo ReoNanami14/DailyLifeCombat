c3739fc112148427cca4e3f4ae06a142b4af8173
DESKTOP-DK0FVIU\faro7 <m0119054af@edu.teu.ac.jp>
2020-09-10 01:17:39 +0900

    足音の素材とサウンドのスクリプト(途中)

---
+++ Assets/SE/GameSe.cs
@@ -0,0 +1,32 @@
+﻿//using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/* 
+public class PlaySE : MonoBehaviour
+{
+    void Start(){}
+
+    void Update(){}
+} 
+*/
+
+namespace SoundSystem
+{
+    public class GameSe :MonoBehaviour
+    {
+        public AudioSource audioSource;
+        public List<AudioClip> audioClipList = new List<AudioClip>();
+
+        public bool IsPaused { get; private set; }
+
+        public void PlaySe(string audioClipName)
+        {
+            if (IsPaused) return;
+
+            AudioClip audioClip = audioClipList.FirstOrDefault(clip => clip.name == audioClipName);
+        }
+
+    }
+}
